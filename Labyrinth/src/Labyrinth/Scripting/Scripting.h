92ac96d4ccaed9677b3c99eca85b240ba8d9dbdc
amayesingnathan <92682126+amayesingnathan@users.noreply.github.com>
2022-07-17 18:15:42 +0100

    Added field and property get and setters
    Also converted Scripting from a namespace of free functions to a static class (so we can have external and internal functions i.e. public/private)

--- Labyrinth/src/Labyrinth/Scripting/Scripting.h
+++ Labyrinth/src/Labyrinth/Scripting/Scripting.h
@@ -18,19 +18,17 @@ namespace Labyrinth {
 		};
 	}
 
-	namespace Scripting {
+	class Scripting
+	{
+	public:
+		static MonoAssembly* LoadCSharpAssembly(const std::string& assemblyPath);
 
-		char* ReadBytes(const std::string& filepath, uint32_t* outSize);
-		MonoAssembly* LoadCSharpAssembly(const std::string& assemblyPath);
-
-		void PrintAssemblyTypes(MonoAssembly* assembly);
-		MonoClass* GetClassInAssembly(MonoAssembly* assembly, const char* namespaceName, const char* className);
-		MonoObject* InstantiateClass(MonoDomain* domain, MonoAssembly* assembly, const char* namespaceName, const char* className);
-
-		void CallMethodInternal(MonoObject* instance, const char* methodName, void** argv, int argc);
+		static void PrintAssemblyTypes(MonoAssembly* assembly);
+		static MonoClass* GetClassInAssembly(MonoAssembly* assembly, const char* namespaceName, const char* className);
+		static MonoObject* InstantiateClass(MonoDomain* domain, MonoAssembly* assembly, const char* namespaceName, const char* className);
 
 		template<typename... Args>
-		void CallMethod(MonoObject* instance, const char* methodName, Args&&... args)
+		static void CallMethod(MonoObject* instance, const char* methodName, Args&&... args)
 		{
 			constexpr size_t argc = sizeof...(Args);
 
@@ -58,10 +56,37 @@ namespace Labyrinth {
 			CallMethodInternal(instance, methodName, voidArgs, argc);
 		}
 
-		uint8_t GetFieldAccessibility(MonoClassField* field);
-		uint8_t GetPropertyAccessibility(MonoProperty* property);
+		template<typename T>
+		static void GetField(MonoObject* instance, const char* fieldName, T& data)
+		{
+			void* voidData = data;
+			GetFieldInternal(instance, fieldName, &voidData);
+			LAB_CORE_ASSERT(voidData, "Field return value from Mono was null!");
+			data = *voidData;
+		}
 
-		bool CheckMonoError(MonoError& error);
+		template<typename T>
+		static T GetProperty(MonoObject* instance, const char* propertyName)
+		{
+			return *Cast<T>(GetPropertyInternal(instance, propertyName));
+		}
+		static MonoObject* GetPropertyRef(MonoObject* instance, const char* propertyName);
 
-	}
+		static void SetField(MonoObject* instance, const char* fieldName, void* data);
+		static void SetProperty(MonoObject* instance, const char* propertyName, void* data);
+
+	private:
+		static char* ReadBytes(const std::string& filepath, uint32_t* outSize);
+
+		static bool CheckMonoError(MonoError& error);
+
+		static void CallMethodInternal(MonoObject* instance, const char* methodName, void** argv, int argc);
+
+		static uint8_t GetFieldAccessibility(MonoClassField* field);
+		static uint8_t GetPropertyAccessibility(MonoProperty* property);
+
+		static void GetFieldInternal(MonoObject* instance, const char* fieldName, void* data);
+		static void* GetPropertyInternal(MonoObject* instance, const char* propertyName);
+
+	};
 }
