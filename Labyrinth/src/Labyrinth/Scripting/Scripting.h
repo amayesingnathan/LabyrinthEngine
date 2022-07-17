a6f0d2c0497afb17288581ff632b77fd07594b46
amayesingnathan <92682126+amayesingnathan@users.noreply.github.com>
2022-07-17 16:01:18 +0100

    Added accessibility and error functions

--- Labyrinth/src/Labyrinth/Scripting/Scripting.h
+++ Labyrinth/src/Labyrinth/Scripting/Scripting.h
@@ -1,28 +1,38 @@
 #pragma once
 
+#include "MonoForward.h"
+
 #include <string>
 
-struct _MonoDomain;
-typedef struct _MonoDomain MonoDomain;
-struct _MonoAssembly;
-typedef struct _MonoAssembly MonoAssembly;
-struct _MonoClass;
-typedef struct _MonoClass MonoClass;
-struct _MonoObject;
-typedef struct _MonoObject MonoObject;
-
 namespace Labyrinth {
+
+	namespace Accessibility
+	{
+		enum : uint8_t
+		{
+			None = 0,
+			Private = (1 << 0),
+			Internal = (1 << 1),
+			Protected = (1 << 2),
+			Public = (1 << 3)
+		};
+	}
 
 	namespace Scripting {
 
 		char* ReadBytes(const std::string& filepath, uint32_t* outSize);
 		MonoAssembly* LoadCSharpAssembly(const std::string& assemblyPath);
+
 		void PrintAssemblyTypes(MonoAssembly* assembly);
 		MonoClass* GetClassInAssembly(MonoAssembly* assembly, const char* namespaceName, const char* className);
-
 		MonoObject* InstantiateClass(MonoDomain* domain, MonoAssembly* assembly, const char* namespaceName, const char* className);
 
 		void CallMethod(MonoObject* instance, const char* methodName, void** argv, int argc);
+
+		uint8_t GetFieldAccessibility(MonoClassField* field);
+		uint8_t GetPropertyAccessibility(MonoProperty* property);
+
+		bool CheckMonoError(MonoError& error);
 
 	}
 }
