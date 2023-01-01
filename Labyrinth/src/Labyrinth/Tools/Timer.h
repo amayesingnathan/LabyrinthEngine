60038ef65913d568d76b9f96bb1959329fe84fe5
amayesingnathan <amayesingnathan123@gmail.com>
2023-01-01 16:40:11 +0000

    Readded timer helper

---
+++ Labyrinth/src/Labyrinth/Tools/Timer.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <chrono>
+
+#include <Labyrinth/Core/System/Base.h>
+
+namespace Laby {
+
+	class Timer
+	{
+	public:
+		Timer()
+		{
+			reset();
+		}
+
+		void reset()
+		{
+			mStart = std::chrono::steady_clock::now();
+		}
+
+		f32 elapsed()
+		{
+			return std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::steady_clock::now() - mStart).count() * 0.001f * 0.001f * 0.001f;
+		}
+
+		f32 elapsedMillis()
+		{
+			return elapsed() * 1000.0f;
+		}
+
+	private:
+		std::chrono::time_point<std::chrono::steady_clock> mStart;
+	};
+
+}
