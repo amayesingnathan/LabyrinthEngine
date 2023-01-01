82324d7a24a7ba06b0dea43a1484db0781cc366a
amayesingnathan <amayesingnathan123@gmail.com>
2023-01-01 16:34:16 +0000

    Renamed static container template parameter

--- Labyrinth/src/Labyrinth/Containers/StaticBuffer.h
+++ Labyrinth/src/Labyrinth/Containers/StaticBuffer.h
@@ -4,25 +4,25 @@
 
 namespace Laby {
 
-	template<usize _Size>
+	template<usize TSize>
 	class StaticBuffer
 	{
 	public:
 		StaticBuffer() = default;
-		StaticBuffer(Byte data[_Size])
+		StaticBuffer(Byte data[TSize])
 		{
-			memset(mData, 0, _Size);
-			memcpy(mData, data, _Size);
+			memset(mData, 0, TSize);
+			memcpy(mData, data, TSize);
 		}
 
 		template<usize _Other>
 		StaticBuffer(const StaticBuffer<_Other>& buffer)
 		{
-			memset(mData, 0, _Size);
-			if constexpr (_Other <= _Size)
+			memset(mData, 0, TSize);
+			if constexpr (_Other <= TSize)
 				memcpy(mData, buffer.mData, _Other);
 			else
-				memcpy(mData, buffer.mData, _Size);
+				memcpy(mData, buffer.mData, TSize);
 		}
 
 		Byte& operator[](usize index)
@@ -35,9 +35,9 @@ namespace Laby {
 			return mData[index];
 		}
 
-		constexpr usize size() const { return _Size; }
+		constexpr usize size() const { return TSize; }
 
 	protected:
-		Byte mData[_Size] = { 0 };
+		Byte mData[TSize] = { 0 };
 	};
 }
--- Labyrinth/src/Labyrinth/Containers/StaticString.h
+++ Labyrinth/src/Labyrinth/Containers/StaticString.h
@@ -4,29 +4,29 @@
 
 namespace Laby {
 
-	template<usize _Size>
-	class StaticString : private StaticBuffer<_Size>
+	template<usize TSize>
+	class StaticString : private StaticBuffer<TSize>
 	{
 	public:
 		StaticString(const std::string& string)
 		{
-			LAB_CORE_ASSERT(string.size() <= _Size);
+			LAB_CORE_ASSERT(string.size() <= TSize);
 
-			memset(mData, 0, _Size);
+			memset(mData, 0, TSize);
 			memcpy(mData, string.c_str(), string.size());;
 		}
 
-		constexpr usize length() const { return _Size; }
+		constexpr usize length() const { return TSize; }
 
 		operator char* ()
 		{
-			LAB_CORE_ASSERT(mData[_Size - 1] == 0); // At least the last character should be null
+			LAB_CORE_ASSERT(mData[TSize - 1] == 0); // At least the last character should be null
 			return (char*)mData;
 		}
 
 		std::string toString() const
 		{
-			LAB_CORE_ASSERT(mData[_Size - 1] == 0); // At least the last character should be null
+			LAB_CORE_ASSERT(mData[TSize - 1] == 0); // At least the last character should be null
 			return (const char*)mData;
 		}
 	};
