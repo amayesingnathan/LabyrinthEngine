5164dbdfbb25ce50987b3d81d48274002df0dbeb
amayesingnathan <amayesingnathan123@gmail.com>
2023-01-01 16:34:59 +0000

    Readded input polling interface

---
+++ Labyrinth/src/Labyrinth/IO/Input.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "Labyrinth/IO/KeyCodes.h"
+#include "Labyrinth/IO/MouseCodes.h"
+
+#include <glm/glm.hpp>
+
+namespace Laby {
+
+	class Input
+	{
+	public:
+		static bool IsWindowHovered();
+
+		static bool IsKeyPressed(KeyCode keycode);
+		static bool IsMouseButtonPressed(MouseCode button);
+
+		static glm::vec2 GetMousePosition();
+		static f32 GetMouseX();
+		static f32 GetMouseY();
+	};
+
+}
