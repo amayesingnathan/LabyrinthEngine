a18bf471db74491e1a62a036f7269b035a94c8f0
amayesingnathan <92682126+amayesingnathan@users.noreply.github.com>
2023-01-20 13:50:21 +0000

    Readded options panel

---
+++ Labyrinth/src/Labyrinth/Editor/Panels/OptionsPanel.cpp
@@ -0,0 +1,22 @@
+#include "Lpch.h"
+#include "OptionsPanel.h"
+
+#include <Labyrinth/ImGui/ImGuiWidgets.h>
+#include <Labyrinth/Scripting/ScriptEngine.h>
+
+namespace Laby {
+
+	void OptionsPanel::onImGuiRender()
+	{
+		if (!mEditorData)
+			return;
+
+		Widgets::Checkbox("Display Colliders", mEditorData->displayColliders);
+		Widgets::ColourEdit("Collider Colour", mEditorData->colliderColour);
+		Widgets::ColourEdit("Selection Colour", mEditorData->selectionColour);
+		Widgets::Button("Reset Camera Position", [this]() { mEditorData->camera.resetPosition(); });
+		Widgets::Button("Reset Camera Angle", [this]() { mEditorData->camera.resetAngle(); });
+		Widgets::Button("Reset Camera Angle", [this]() { mEditorData->camera.resetAngle(); });
+		Widgets::Button("Regenerate Script Project", ScriptEngine::RegenScriptProject);
+	}
+}
