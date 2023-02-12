8385eb312a7256f83baae8c749bfb660c54458b0
amayesingnathan <amayesingnathan123@gmail.com>
2023-02-12 15:55:57 +0000

    Readded single header include details

--- Labyrinth/src/Labyrinth.h
+++ Labyrinth/src/Labyrinth.h
@@ -1,53 +1,52 @@
-//#pragma once
-//
-////For use by Labyrinth applications
-//
-//#include "Labyrinth/Core/System/Base.h"
-//#include "Labyrinth/Core/System/Log.h"
-//#include "Labyrinth/Core/System/Assert.h"
-//#include "Labyrinth/Core/System/Ref.h"
-//#include "Labyrinth/Core/System/Cast.h"
-//#include "Labyrinth/Core/System/Types.h"
-//
-//#include "Labyrinth/Core/Application.h"
-//#include "Labyrinth/Core/Layer.h"
-//#include "Labyrinth/Core/Timestep.h"
-//#include "Labyrinth/Core/CameraController.h"
-//
-//#include <Labyrinth/Containers/Buffer.h>
-//#include <Labyrinth/Containers/StaticString.h>
-//
-//#include "Labyrinth/IO/Input.h"
-//#include "Labyrinth/IO/KeyCodes.h"
-//#include "Labyrinth/IO/MouseButtonCodes.h"
-//#include "Labyrinth/IO/JSON.h"
-//
-//#include "Labyrinth/Maths/Constant.h"
-//
-//#include "Labyrinth/ImGui/ImGuiLayer.h"
-//
-//#include "Labyrinth/Scene/Scene.h"
-//#include "Labyrinth/Scene/Entity.h"
-//#include "Labyrinth/Scene/Components.h"
-//#include "Labyrinth/Scene/SceneSerialiser.h"
-//
-//#include "Labyrinth/Scripting/NativeScript.h"
-//
-//#include "Labyrinth/Assets/AssetManager.h"
-//
-//// ---Renderer------------------------
-//#include "Labyrinth/Renderer/Renderer.h"
-//#include "Labyrinth/Renderer/Renderer2D.h"
-//#include "Labyrinth/Renderer/RenderCommand.h"
-//
-//#include "Labyrinth/Renderer/Buffer.h"
-//#include "Labyrinth/Renderer/Shader.h"
-//#include "Labyrinth/Renderer/Framebuffer.h"
-//#include "Labyrinth/Renderer/Texture.h"
-//#include "Labyrinth/Renderer/SubTexture.h"
-//#include "Labyrinth/Renderer/VertexArray.h"
-//
-//#include "Labyrinth/Renderer/Camera.h"
-//// -----------------------------------
-//
-//#include "Labyrinth/Tools/Timer.h"
+#pragma once
+
+//For use by Labyrinth applications
+
+#include <Labyrinth/Core/Application.h>
+#include <Labyrinth/Core/UUID.h>
+
+#include <Labyrinth/Assets/AssetManager.h>
+
+#include <Labyrinth/Containers/Buffer.h>
+#include <Labyrinth/Containers/Grid.h>
+#include <Labyrinth/Containers/StaticString.h>
+#include <Labyrinth/Containers/TSQueue.h>
+
+#include <Labyrinth/ImGui/ImGuiWidgets.h>
+
+#include <Labyrinth/IO/Input.h>
+#include <Labyrinth/IO/Filesystem.h>
+#include <Labyrinth/IO/KeyCodes.h>
+#include <Labyrinth/IO/MouseCodes.h>
+#include <Labyrinth/IO/JSON.h>
+#include <Labyrinth/IO/YAML.h>
+
+#include <Labyrinth/Networking/ClientLayer.h>
+#include <Labyrinth/Networking/ServerLayer.h>
+
+#include <Labyrinth/Project/Project.h>
+
+#include "Labyrinth/Renderer/Renderer.h"
+#include "Labyrinth/Renderer/Renderer2D.h"
+#include "Labyrinth/Renderer/Buffer.h"
+#include "Labyrinth/Renderer/Shader.h"
+#include "Labyrinth/Renderer/Framebuffer.h"
+#include "Labyrinth/Renderer/Texture.h"
+#include "Labyrinth/Renderer/SubTexture.h"
+#include "Labyrinth/Renderer/Camera.h"
+
+#include "Labyrinth/Scene/Scene.h"
+#include "Labyrinth/Scene/Entity.h"
+#include "Labyrinth/Scene/Components.h"
+#include "Labyrinth/Scene/SceneSerialiser.h"
+
+#include "Labyrinth/Scripting/MarshalUtils.h"
+#include "Labyrinth/Scripting/ScriptEngine.h"
+#include "Labyrinth/Scripting/ScriptCache.h"
+
+#include <Labyrinth/Tilemap/Tilemap.h>
+
+#include "Labyrinth/Tools/EnumUtils.h"
+#include "Labyrinth/Tools/MathUtils.h"
+#include "Labyrinth/Tools/StringUtils.h"
+#include "Labyrinth/Tools/Timing.h"
