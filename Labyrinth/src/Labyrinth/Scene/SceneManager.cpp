39d895c0c3d0e57961192d794cbce267229c73e7
amayesingnathan <amayesingnathan123@gmail.com>
2023-05-21 13:03:19 +0100

    Add cpp file for SceneManager

---
+++ Labyrinth/src/Labyrinth/Scene/SceneManager.cpp
@@ -0,0 +1,128 @@
+#include "Lpch.h"
+#include "SceneManager.h"
+
+namespace Laby {
+
+    void SceneManager::reset()
+    {
+        mScenes.clear();
+        mSceneLoaders.clear();
+        mCurrentScene = 0;
+        mLoadingScene = 0;
+    }
+
+    UUID SceneManager::preload(const fs::path& scenePath)
+    {
+        UUID newScene;
+        mSceneLoaders.emplace(newScene, scenePath);
+        mLoadingScene = newScene;
+
+        return newScene;
+    }
+
+    void SceneManager::load()
+    {
+        if (!mLoadingScene)
+        {
+            LAB_CORE_WARN("No scene was preloaded to load now!");
+            return;
+        }
+        if (!mSceneLoaders.contains(mLoadingScene))
+        {
+            LAB_CORE_ERROR("This scene loader could not be found for the in progress scene!");
+            return;
+        }
+
+        mScenes[mLoadingScene] = mSceneLoaders[mLoadingScene].get();
+        mSceneLoaders.erase(mLoadingScene);
+        SetCurrentScene(mLoadingScene);
+        mLoadingScene = 0;
+    }
+
+    void SceneManager::load(UUID sceneID)
+    {
+        bool preloaded = mSceneLoaders.contains(sceneID);
+        bool loaded = mScenes.contains(sceneID);
+
+        if (!preloaded && !loaded)
+        {
+            LAB_CORE_ERROR("This scene does not exist in the scene manager!");
+            return;
+        }
+
+        if (preloaded)
+        {
+            mScenes[sceneID] = mSceneLoaders[sceneID].get();
+            mSceneLoaders.erase(sceneID);
+        }
+
+        SetCurrentScene(sceneID);
+    }
+
+    UUID SceneManager::load(const fs::path& scenePath)
+    {
+        UUID newSceneID;
+        mScenes[newSceneID] = SceneLoader(scenePath).get();
+        SetCurrentScene(newSceneID);
+        return newSceneID;
+    }
+
+    UUID SceneManager::loadClone(UUID cloneID)
+    {
+        UUID newSceneID;
+        Ref<Scene> toClone = mScenes[cloneID];
+        mScenes[newSceneID] = SceneUtils::Clone(toClone);
+        SetCurrentScene(newSceneID);
+        return newSceneID;
+    }
+
+    void SceneManager::unload(UUID id)
+    {
+        mScenes.erase(id);
+    }
+
+    Ref<Scene> SceneManager::get(UUID id) const
+    {
+        if (!mScenes.contains(id))
+        {
+            LAB_CORE_ERROR("Scene {} does not exist in scene manager!", id);
+            return nullptr;
+        }
+
+        return mScenes.at(id);
+    }
+
+    Ref<Scene> SceneManager::getActive() const
+    {
+        if (!mCurrentScene)
+            return nullptr;
+
+        if (!mScenes.contains(mCurrentScene))
+        {
+            LAB_CORE_ERROR("Currently active scene {} does not exist in scene manager!", mCurrentScene);
+            return nullptr;
+        }
+
+        return mScenes.at(mCurrentScene);
+    }
+
+    UUID SceneManager::newScene()
+    {
+        UUID newScene;
+        mScenes[newScene] = Ref<Scene>::Create();
+        return newScene;
+    }
+
+    void SceneManager::addSceneChangeCallback(Action<Ref<Scene>>&& action)
+    {
+        mOnChangeCallbacks.emplace_back(std::move(action));
+    }
+
+    void SceneManager::SetCurrentScene(UUID scene)
+    {
+        for (const auto& action : mOnChangeCallbacks)
+            action(mScenes[scene]);
+
+        mCurrentScene = scene;
+    }
+}
--- Labyrinth/src/Labyrinth/Scene/SceneManager.h
+++ Labyrinth/src/Labyrinth/Scene/SceneManager.h
@@ -11,130 +11,25 @@ namespace Laby {
 		SceneManager(const SceneManager&) = delete;
 		SceneManager& operator=(const SceneManager&) = delete;
 
-		void reset()
-		{
-			mScenes.clear();
-			mSceneLoaders.clear();
-			mCurrentScene = 0;
-			mLoadingScene = 0;
-		}
+		void reset();
 
-		UUID preload(const fs::path& scenePath)
-		{
-			UUID newScene;
-			mSceneLoaders.emplace(newScene, scenePath);
-			mLoadingScene = newScene;
+		UUID preload(const fs::path& scenePath);
+		void unload(UUID id);
 
-			return newScene;
-		}
+		void load();
+		void load(UUID sceneID);
+		UUID load(const fs::path& scenePath);
+		UUID loadClone(UUID cloneID);
 
-		void load()
-		{
-			if (!mLoadingScene)
-			{
-				LAB_CORE_WARN("No scene was preloaded to load now!");
-				return;
-			}
-			if (!mSceneLoaders.contains(mLoadingScene))
-			{
-				LAB_CORE_ERROR("This scene loader could not be found for the in progress scene!");
-				return;
-			}
+		Ref<Scene> get(UUID id) const;
+		Ref<Scene> getActive() const;
 
-			mScenes[mLoadingScene] = mSceneLoaders[mLoadingScene].get();
-			mSceneLoaders.erase(mLoadingScene);
-			SetCurrentScene(mLoadingScene);
-			mLoadingScene = 0;
-		}
+		UUID newScene();
 
-		Ref<Scene> load(UUID sceneID)
-		{
-			bool preloaded = mSceneLoaders.contains(sceneID);
-			bool loaded = mScenes.contains(sceneID);
-
-			if (!preloaded && !loaded)
-			{
-				LAB_CORE_ERROR("This scene does not exist in the scene manager!");
-				return nullptr;
-			}
-
-			if (preloaded)
-			{
-				mScenes[sceneID] = mSceneLoaders[sceneID].get();
-				mSceneLoaders.erase(sceneID);
-			}
-
-			SetCurrentScene(sceneID);
-			return mScenes[mCurrentScene];
-		}
-
-		UUID load(const fs::path& scenePath)
-		{
-			UUID newSceneID;
-			mScenes[newSceneID] = SceneLoader(scenePath).get();
-			SetCurrentScene(newSceneID);
-			return newSceneID;
-		}
-
-		UUID loadClone(UUID cloneID)
-		{
-			UUID newSceneID;
-			Ref<Scene> toClone = mScenes[cloneID];
-			mScenes[newSceneID] = SceneUtils::Clone(toClone);
-			SetCurrentScene(newSceneID);
-			return newSceneID;
-		}
-
-		void unload(UUID id)
-		{
-			mScenes.erase(id);
-		}
-
-		Ref<Scene> get(UUID id) const
-		{
-			if (!mScenes.contains(id))
-			{
-				LAB_CORE_ERROR("Scene {} does not exist in scene manager!", id);
-				return nullptr;
-			}
-
-			return mScenes.at(id);
-		}
-
-		Ref<Scene> getActive() const
-		{
-			if (!mCurrentScene)
-				return nullptr;
-
-			if (!mScenes.contains(mCurrentScene))
-			{
-				LAB_CORE_ERROR("Currently active scene {} does not exist in scene manager!", mCurrentScene);
-				return nullptr;
-			}
-
-			return mScenes.at(mCurrentScene);
-		}
-
-		UUID newScene()
-		{
-			UUID newScene;
-			mScenes[newScene] = Ref<Scene>::Create();
-			return newScene;
-		}
-
-		void addSceneChangeCallback(Action<Ref<Scene>>&& action)
-		{
-			mOnChangeCallbacks.emplace_back(std::move(action));
-		}
+		void addSceneChangeCallback(Action<Ref<Scene>>&& action);
 
 	private:
-		void SetCurrentScene(UUID scene)
-		{
-			for (const auto& action : mOnChangeCallbacks)
-				action(mScenes[scene]);
-
-			mCurrentScene = scene;
-		}
+		void SetCurrentScene(UUID scene);
 
 	private:
 		UUID mCurrentScene = 0, mLoadingScene = 0;
