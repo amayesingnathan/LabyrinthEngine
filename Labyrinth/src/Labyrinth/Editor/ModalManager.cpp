3a78d6b471499402ee0f7c01e334c76dc6858a43
amayesingnathan <amayesingnathan123@gmail.com>
2023-05-29 18:58:10 +0100

    Fixed modal window button placement

--- Labyrinth/src/Labyrinth/Editor/ModalManager.cpp
+++ Labyrinth/src/Labyrinth/Editor/ModalManager.cpp
@@ -23,8 +23,13 @@ namespace Laby {
             Utils::SetNextWindowPos<glm::vec2>(Utils::GetMainWindowCentre<glm::vec2>(), { 0.5f, 0.5f });
             if (Widgets::BeginWindow(modalData.heading, &modalData.open, ImGuiWindowFlags_NoDocking))
             {
+                Widgets::BeginChild("ModalBody", glm::vec2{ 0, -2 * Utils::FrameHeightWithSpacing()});
                 modalData.modal->onImGuiRender();
+                Widgets::EndChild();
+
+                Widgets::BeginChild("ModalButtons");
                 RenderButtons(modalData);
+                Widgets::EndChild();
             }
 
             Widgets::EndWindow();
